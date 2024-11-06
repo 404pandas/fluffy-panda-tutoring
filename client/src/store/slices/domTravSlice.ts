585a0eeb0063c0a01ebe68da4354a0ccdecb5e8d
404pandas <mary.panda.jackson@gmail.com>
2024-11-06 09:36:43 -0500

    Someone save me from this pain

--- client/src/store/slices/domTravSlice.ts
+++ client/src/store/slices/domTravSlice.ts
@@ -4,13 +4,19 @@ interface AnimalPosition {
   row: number;
   col: number;
 }
-
+interface Obstacle {
+  row: number;
+  col: number;
+}
 interface GameState {
   rows: number[];
   animalPosition: AnimalPosition;
   errorMessage: string | null;
   maxRows: number;
   maxCols: number;
+  obstacleCount: number;
+  areObstaclesAnimated: boolean;
+  obstacles: Obstacle[];
 }
 
 const initialState: GameState = {
@@ -19,12 +25,24 @@ const initialState: GameState = {
   errorMessage: null,
   maxRows: 3,
   maxCols: 12,
+  obstacleCount: 3,
+  areObstaclesAnimated: false,
+  obstacles: [],
 };
+
+const getRandomCol = (maxCols: number) =>
+  Math.floor(Math.random() * (maxCols - 1)) + 1;
 
 const domTravSlice = createSlice({
   name: "game",
   initialState,
   reducers: {
+    initializeObstacles: (state) => {
+      state.obstacles = Array.from({ length: state.obstacleCount }, () => ({
+        row: Math.floor(Math.random() * Math.floor(state.maxRows / 2)) * 2 + 2,
+        col: getRandomCol(state.maxCols),
+      }));
+    },
     moveAnimal: (state, action: PayloadAction<AnimalPosition>) => {
       const { row, col } = action.payload;
       const isInvalidRow = row % 2 == 0 || row > state.maxRows || row < 0;
@@ -49,6 +67,7 @@ const domTravSlice = createSlice({
         ];
         state.rows = [...state.rows, ...newRows];
         state.maxRows += 3;
+        state.obstacleCount += 3;
       } else {
         state.errorMessage = "You have reached the maximum number of rows.";
       }
@@ -66,6 +85,36 @@ const domTravSlice = createSlice({
 
       state.rows = state.rows.slice(0, -3);
       state.maxRows -= 3;
+      state.obstacleCount -= 3;
+    },
+    addObstacle: (state) => {
+      const rowOptions = state.rows.filter(
+        (row) => row % 2 === 1 && row > state.animalPosition.row
+      );
+      const randomRow =
+        rowOptions[Math.floor(Math.random() * rowOptions.length)];
+      const randomCol = getRandomCol(state.maxCols);
+
+      state.obstacles.push({ row: randomRow, col: randomCol });
+      state.obstacleCount++;
+    },
+    removeObstacle: (state) => {
+      const aboveRows = state.obstacles.filter(
+        (obstacle) => obstacle.row > state.animalPosition.row
+      );
+
+      if (aboveRows.length > 0) {
+        const obstacleToRemove =
+          aboveRows[Math.floor(Math.random() * aboveRows.length)];
+        state.obstacles = state.obstacles.filter(
+          (obs) =>
+            !(
+              obs.row === obstacleToRemove.row &&
+              obs.col === obstacleToRemove.col
+            )
+        );
+        state.obstacleCount--;
+      }
     },
     clearError: (state) => {
       state.errorMessage = null;
@@ -76,9 +125,33 @@ const domTravSlice = createSlice({
     setMaxCols: (state, action: PayloadAction<number>) => {
       state.maxCols = action.payload;
     },
+    setObstacleAnimation: (state, action: PayloadAction<boolean>) => {
+      state.areObstaclesAnimated = action.payload;
+    },
+    setObstacleCount: (state, action: PayloadAction<number>) => {
+      const newCount = action.payload;
+      if (newCount < state.obstacleCount) {
+        const currentRow = state.animalPosition.row;
+        const allowedRowsAbove = state.rows.filter(
+          (row) => row > currentRow && row % 2 === 1
+        );
+        state.obstacleCount = Math.min(newCount, allowedRowsAbove.length);
+      } else {
+        state.obstacleCount = newCount;
+      }
+    },
   },
 });
 
-export const { moveAnimal, addRow, removeRow, clearError } =
-  domTravSlice.actions;
+export const {
+  moveAnimal,
+  addRow,
+  removeRow,
+  clearError,
+  setObstacleAnimation,
+  setObstacleCount,
+  addObstacle,
+  removeObstacle,
+  initializeObstacles,
+} = domTravSlice.actions;
 export default domTravSlice.reducer;
